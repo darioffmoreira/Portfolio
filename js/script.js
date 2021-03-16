240d9af22477c8f6e77bb2d5d84eef76b6ded73c
darioffmoreira <darioffmoreira@gmail.com>
2021-03-16 22:58:08 +0000

    JS > Anchors on menu - ok

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', function(){
     document.body.classList.toggle('nav-open');
     console.log('its ok, works!');
 });
+
+navLinks.forEach(function(link) {
+    link.addEventListener('click', function(){
+        document.body.classList.remove('nav-open');
+        console.log('its ok, worksssss!');
+    })
+})
