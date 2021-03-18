428d98258b6ab3d7d989b337a79ca0138368d679
darioffmoreira <darioffmoreira@gmail.com>
2021-03-18 23:38:02 +0000

    script commented

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav__link');
 
-navToggle.addEventListener('click', function(){
-    document.body.classList.toggle('nav-open');
-    console.log('its ok, works!');
-});
+// navToggle.addEventListener('click', function(){
+//     document.body.classList.toggle('nav-open');
+//     console.log('its ok, works!');
+// });
 
-navLinks.forEach(function(link) {
-    link.addEventListener('click', function(){
-        document.body.classList.remove('nav-open');
-        console.log('its ok, worksssss!');
-    })
-})
+// navLinks.forEach(function(link) {
+//     link.addEventListener('click', function(){
+//         document.body.classList.remove('nav-open');
+//         console.log('its ok, worksssss!');
+//     })
+// })
